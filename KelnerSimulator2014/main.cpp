8c3ea30c3696c33dfaf9e39ca1745ce414fdf82d
Greg Bak <gregbak@me.com>
2014-10-17 15:00:17 +0200

    i twerks

--- KelnerSimulator2014/main.cpp
+++ KelnerSimulator2014/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 using namespace std;
+char n;
 
 int main()
 {
-	bool shit; // Booyah!
+	cout << "Interpreter komend kelnera v0.Pierdyliard\nWpisz komendy:\n";
+	while (cin >> n && n != 'q'){ 
+		cout << n << endl;
+	}
 	cout << "Hello World!" << endl;
 	return 0;
 }
