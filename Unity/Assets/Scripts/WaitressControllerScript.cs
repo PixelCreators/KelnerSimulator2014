4e0786af233e3e70ba078cc9dd3b04b5174a24a7
Gregorein <szary.elf@gmail.com>
2014-12-18 01:19:33 +0100

    prep text control

--- Unity/Assets/Scripts/WaitressControllerScript.cs
+++ Unity/Assets/Scripts/WaitressControllerScript.cs
@@ -38,6 +38,35 @@ public class WaitressControllerScript : MonoBehaviour
             moveTowards();
             Debug.Log("Wykonano polecenie moveTowards!");
         }
+		//(opcja) podmiot (opcja) czynność (opcja + opcja)
+		/**
+
+		int iKelner;
+		var commandChain:string[] = command.Split(" ");
+
+		if (commandChain[0].Equals("niech")) {
+			if(commandChain[1].Equals("każdy") && commandChain[2].Equals("kelner")) {
+				iKelner = -1;
+			}
+			else if(!commandChain[1].Equals("kelner") || !int.TryParse(commandChain[2], out iKelner)) {
+				iKelner = 0; // nie można sparsować, w innym wypadku już podstawia
+			}
+			if(commandChain[3].Equals("zmywaj")) {
+				//call kelner zmywaj(iKelner)
+			}
+			else if(commandChain[3].Equals("poda") && commandChain[5].Equals("do") && commandChain[6].Equals("stołu")) {
+				//call kelner danie stół podaj(iKelner, commandChain[4], int.Parse(commandChain[7])
+			}
+			else if(commandChain[3].Equals("sprzątnie") && commandChain[4].Equals("stół")) {
+				//call kelner stół sprzątnij(iKelner, int.Parse(commandChain[5]))
+				// jeśli brak id stołu to sprzątnij wszystkie stoły 
+			}y
+			else if(commandChain[3].Equals("odbierze") && commandChain[4].Equals("zamówienie") && commandChain[5].Equals("od") && commandChain[6].Equals("stołu")) {
+				//call kelner stół odbierz(iKelner, int.Parse(commandChain[7]))
+				// jeśli brak id stołu to odbierz zamówienia ze wszystkich stołów
+			}
+		}
+		/**/
     }
 
     public void interpretCommand()
