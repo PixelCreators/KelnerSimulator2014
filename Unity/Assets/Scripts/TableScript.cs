2286cdb7b2a3b579f01ddb6f17f3fdf41f36995a
mathewp <mateusz.pusty@gmail.com>
2015-01-14 21:24:46 +0100

    Dodano funkcje  wyłączania gości i stany stolika.

--- Unity/Assets/Scripts/TableScript.cs
+++ Unity/Assets/Scripts/TableScript.cs
@@ -3,6 +3,22 @@ using System.Collections;
 
 public class TableScript : MonoBehaviour
 {
+    private enum TableState
+    {
+        Free,
+        Busy
+    }
+
+    private enum TableStatus
+    {
+        ChoosingOrder,
+        OrderReady,
+        WaitingForOrder,
+        ProgressingOrder,
+        Dirty,
+        Clean,
+    }
+
     //Komponenty
     private Transform transformComponent;
 
@@ -11,19 +27,51 @@ public class TableScript : MonoBehaviour
     //private GameObject dishesModule; //To be implemented
 
 
+    //Zmienne publicze
+    public float minTimeAftMealSrvd, maxTimeAftMealServ;
+    
+    //Zmienne prywatne
+    private bool mealServed = true;
+    private TableState state;
+    private TableStatus status;
+
     void Awake()
     {
         transformComponent = transform;
         peopleModule = transformComponent.FindChild("People").gameObject;
+
+        state = TableState.Free;
+        status = TableStatus.Clean;
     }
 
     void Start()
     {
-        peopleModule.SetActive(false);
+        
     }
 
     void Update()
     {
-
+            
     }
+
+    void setGuestsVisible(bool set)
+    {
+        peopleModule.SetActive(set);
+    }
+
+    IEnumerator ChangeGuestsStatus()
+    {
+        if (mealServed)
+        {
+            yield return new WaitForSeconds(getRandomTime(minTimeAftMealSrvd, maxTimeAftMealServ));
+            peopleModule.SetActive(false);
+        }
+    }
+
+    float getRandomTime(float min, float max)
+    {
+        return Random.Range(min, max);
+    }
+
+
 }
