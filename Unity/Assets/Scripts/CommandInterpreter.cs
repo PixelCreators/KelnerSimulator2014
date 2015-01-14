9d77799a9e09cc9bb81d8752c77da0a4bd93b482
mathewp <mateusz.pusty@gmail.com>
2015-01-15 00:00:48 +0100

    AI Stolika działa.

--- Unity/Assets/Scripts/CommandInterpreter.cs
+++ Unity/Assets/Scripts/CommandInterpreter.cs
@@ -12,6 +12,9 @@ namespace Assets.Scripts
         public Text lastCommandText;
 
         public InterpreterEngine interpreter;
+        private List<List<string>> dictionary;
+        private List<List<string>> cookbook;
+        public int CookbookSize = 0;
 
         private string lastCommand;
 
@@ -21,25 +24,29 @@ namespace Assets.Scripts
             lastCommandText = GameObject.Find("LastCommand").GetComponent<Text>();
         }
 
+        void Start()
+        {
+            Debug.Log("debug : new interpreter");
+            interpreter = new InterpreterEngine();
+
+            Debug.Log("debug : parse dictionary");
+            dictionary = new List<List<string>>(interpreter.readFile("słownik.txt"));
+
+            Debug.Log("debug : parse cookbook");
+            cookbook = new List<List<string>>(interpreter.readFile("potrawy.txt"));
+            CookbookSize = cookbook.Count;
+        }
+
         public void interpretCommand()
         {
             getCommandFromInput();
             invokeCommand();
             showLastCommand();
+
         }
 
         public void invokeCommand()
         {
-			Debug.Log("debug : new interpreter");
-            interpreter = new InterpreterEngine();
-
-			Debug.Log("debug : parse dictionary");
-            List<List<string>> dictionary = new List<List<string>>(interpreter.readFile("słownik.txt"));
-
-			Debug.Log("debug : parse cookbook");
-            List<List<string>> cookbook = new List<List<string>>(interpreter.readFile("potrawy.txt"));
-
-
             List<int> commandTranslation = new List<int>();
 			commandTranslation = interpreter.parseInput(lastCommand, dictionary, cookbook);
 
