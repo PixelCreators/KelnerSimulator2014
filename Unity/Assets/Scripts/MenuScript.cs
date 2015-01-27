1ecb3d3bcaf3f770f1ad7a8abaea99b7a5af0095
Nimbus2014 <weronikasieinska@wp.pl>
2015-01-28 00:13:03 +0100

    Dodalam teczusiowe menu :) #krainakotorozcow

---
+++ Unity/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartButton()
+    {
+        Application.LoadLevel("TestLevel");
+    }
+
+    public void AuthorsButton()
+    {
+        Application.LoadLevel("Authors");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void BackButton()
+    {
+        Application.LoadLevel("Menu");
+    }
+}
