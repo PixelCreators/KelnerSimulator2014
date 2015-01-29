f335fd327277ad754ad28f5ec3964ea786954ad6
XIIIvs <xiii_1994@wp.pl>
2015-01-29 02:33:58 +0100

    Skrypt wczytujący ścieżki wraz z plikami ścieżek

---
+++ Unity/Assets/Scripts/AddPaths.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class AddPaths : MonoBehaviour
+    {
+
+        public string fileName;
+        WaitressControllerScript waitress;
+
+        // Use this for initialization
+        void Start()
+        {
+            waitress = gameObject.GetComponent<WaitressControllerScript>();
+
+            int destination = 0;
+            StreamReader stream = new StreamReader(Application.dataPath + "/" + fileName);
+            while (!stream.EndOfStream)
+            {
+                string line = stream.ReadLine();
+                string[] splitArray = line.Split(' ');
+                foreach (string s in splitArray)
+                {
+                    string tmp = "Waypoint" + s;
+                    Transform waypoint = GameObject.Find(tmp).GetComponent<Transform>();
+                    waitress.avaliblePaths[destination].pathPoints.Add(waypoint);
+                    Debug.Log("Add to destination " + destination + " waypoint: " + tmp);
+                }
+                destination++;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
